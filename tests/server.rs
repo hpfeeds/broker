6e4b44460824706cba6cfafcc8501f49a95ac60f
John Carr <john.carr@unrouted.co.uk>
2022-11-25 13:48:50 +0000

    Test for sub/unsub

--- tests/server.rs
+++ tests/server.rs
@@ -6,334 +6,6 @@ use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
 // use tokio::time::{self, Duration};
 
-#[tokio::test]
-async fn pub_sub() {
-    let addr = start_server().await;
-
-    let mut conn = Connection::new(TcpStream::connect(addr).await.unwrap());
-    let payload: &[u8; 21] = b"this is a byte string";
-    conn.write_frame(&Frame::Publish {
-        ident: "foo".into(),
-        channel: "bar".into(),
-        payload: payload.to_vec().into(),
-    })
-    .await
-    .unwrap();
-
-    let mut sub1 = Connection::new(TcpStream::connect(addr).await.unwrap());
-    sub1.read_frame().await.unwrap();
-
-    sub1.write_frame(&Frame::Subscribe {
-        ident: "sub1".into(),
-        channel: "bar".into(),
-    })
-    .await
-    .unwrap();
-
-    let payload: &[u8; 21] = b"this is a byte strin2";
-    conn.write_frame(&Frame::Publish {
-        ident: "foo".into(),
-        channel: "bar".into(),
-        payload: payload.to_vec().into(),
-    })
-    .await
-    .unwrap();
-
-    match sub1.read_frame().await {
-        Ok(Some(Frame::Publish {
-            ident,
-            channel,
-            payload,
-        })) => {
-            assert_eq!(ident, "foo");
-            assert_eq!(channel, "bar");
-            assert_eq!(payload, Bytes::from_static(b"this is a byte strin2"));
-        }
-        Ok(Some(frame)) => {
-            panic!("Unexpected frame: {}", frame);
-        }
-        _ => {
-            panic!("Unexpected")
-        }
-    }
-
-    /*
-    // Publish a message, there are no subscribers yet so the server will
-    // return `0`.
-    publisher
-    .write_all(b"*3\r\n$7\r\nPUBLISH\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
-    .await
-    .unwrap();
-
-    let mut response = [0; 4];
-    publisher.read_exact(&mut response).await.unwrap();
-    assert_eq!(b":0\r\n", &response);
-
-    // Create a subscriber. This subscriber will only subscribe to the `hello`
-    // channel.
-    let mut sub1 = TcpStream::connect(addr).await.unwrap();
-    sub1.write_all(b"*2\r\n$9\r\nSUBSCRIBE\r\n$5\r\nhello\r\n")
-    .await
-    .unwrap();
-
-    // Read the subscribe response
-    let mut response = [0; 34];
-    sub1.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$9\r\nsubscribe\r\n$5\r\nhello\r\n:1\r\n"[..],
-        &response[..]
-    );
-
-    // Publish a message, there now is a subscriber
-    publisher
-    .write_all(b"*3\r\n$7\r\nPUBLISH\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
-    .await
-    .unwrap();
-
-    let mut response = [0; 4];
-    publisher.read_exact(&mut response).await.unwrap();
-    assert_eq!(b":1\r\n", &response);
-
-    // The first subscriber received the message
-    let mut response = [0; 39];
-    sub1.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$7\r\nmessage\r\n$5\r\nhello\r\n$5\r\nworld\r\n"[..],
-        &response[..]
-    );
-
-    // Create a second subscriber
-    //
-    // This subscriber will be subscribed to both `hello` and `foo`
-    let mut sub2 = TcpStream::connect(addr).await.unwrap();
-    sub2.write_all(b"*3\r\n$9\r\nSUBSCRIBE\r\n$5\r\nhello\r\n$3\r\nfoo\r\n")
-    .await
-    .unwrap();
-
-    // Read the subscribe response
-    let mut response = [0; 34];
-    sub2.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$9\r\nsubscribe\r\n$5\r\nhello\r\n:1\r\n"[..],
-        &response[..]
-    );
-    let mut response = [0; 32];
-    sub2.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$9\r\nsubscribe\r\n$3\r\nfoo\r\n:2\r\n"[..],
-        &response[..]
-    );
-
-    // Publish another message on `hello`, there are two subscribers
-    publisher
-    .write_all(b"*3\r\n$7\r\nPUBLISH\r\n$5\r\nhello\r\n$5\r\njazzy\r\n")
-    .await
-    .unwrap();
-
-    let mut response = [0; 4];
-    publisher.read_exact(&mut response).await.unwrap();
-    assert_eq!(b":2\r\n", &response);
-
-    // Publish a message on `foo`, there is only one subscriber
-    publisher
-    .write_all(b"*3\r\n$7\r\nPUBLISH\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
-    .await
-    .unwrap();
-
-    let mut response = [0; 4];
-    publisher.read_exact(&mut response).await.unwrap();
-    assert_eq!(b":1\r\n", &response);
-
-    // The first subscriber received the message
-    let mut response = [0; 39];
-    sub1.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$7\r\nmessage\r\n$5\r\nhello\r\n$5\r\njazzy\r\n"[..],
-        &response[..]
-    );
-
-    // The second subscriber received the message
-    let mut response = [0; 39];
-    sub2.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$7\r\nmessage\r\n$5\r\nhello\r\n$5\r\njazzy\r\n"[..],
-        &response[..]
-    );
-
-    // The first subscriber **did not** receive the second message
-    let mut response = [0; 1];
-    time::timeout(Duration::from_millis(100), sub1.read(&mut response))
-    .await
-    .unwrap_err();
-
-    // The second subscriber **did** receive the message
-    let mut response = [0; 35];
-    sub2.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$7\r\nmessage\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"[..],
-        &response[..]
-    );
-    */
-}
-
-/*
-
-#[tokio::test]
-async fn manage_subscription() {
-    let addr = start_server().await;
-
-    let mut publisher = TcpStream::connect(addr).await.unwrap();
-
-    // Create a subscriber
-    let mut sub = TcpStream::connect(addr).await.unwrap();
-    sub.write_all(b"*2\r\n$9\r\nSUBSCRIBE\r\n$5\r\nhello\r\n")
-        .await
-        .unwrap();
-
-    // Read the subscribe response
-    let mut response = [0; 34];
-    sub.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$9\r\nsubscribe\r\n$5\r\nhello\r\n:1\r\n"[..],
-        &response[..]
-    );
-
-    // Update subscription to add `foo`
-    sub.write_all(b"*2\r\n$9\r\nSUBSCRIBE\r\n$3\r\nfoo\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 32];
-    sub.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$9\r\nsubscribe\r\n$3\r\nfoo\r\n:2\r\n"[..],
-        &response[..]
-    );
-
-    // Update subscription to remove `hello`
-    sub.write_all(b"*2\r\n$11\r\nUNSUBSCRIBE\r\n$5\r\nhello\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 37];
-    sub.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$11\r\nunsubscribe\r\n$5\r\nhello\r\n:1\r\n"[..],
-        &response[..]
-    );
-
-    // Publish a message to `hello` and then a message to `foo`
-    publisher
-        .write_all(b"*3\r\n$7\r\nPUBLISH\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
-        .await
-        .unwrap();
-    let mut response = [0; 4];
-    publisher.read_exact(&mut response).await.unwrap();
-    assert_eq!(b":0\r\n", &response);
-
-    publisher
-        .write_all(b"*3\r\n$7\r\nPUBLISH\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
-        .await
-        .unwrap();
-    let mut response = [0; 4];
-    publisher.read_exact(&mut response).await.unwrap();
-    assert_eq!(b":1\r\n", &response);
-
-    // Receive the message
-    // The second subscriber **did** receive the message
-    let mut response = [0; 35];
-    sub.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$7\r\nmessage\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"[..],
-        &response[..]
-    );
-
-    // No more messages
-    let mut response = [0; 1];
-    time::timeout(Duration::from_millis(100), sub.read(&mut response))
-        .await
-        .unwrap_err();
-
-    // Unsubscribe from all channels
-    sub.write_all(b"*1\r\n$11\r\nunsubscribe\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 35];
-    sub.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        &b"*3\r\n$11\r\nunsubscribe\r\n$3\r\nfoo\r\n:0\r\n"[..],
-        &response[..]
-    );
-}
-
-// In this case we test that server Responds with an Error message if a client
-// sends an unknown command
-#[tokio::test]
-async fn send_error_unknown_command() {
-    let addr = start_server().await;
-
-    // Establish a connection to the server
-    let mut stream = TcpStream::connect(addr).await.unwrap();
-
-    // Get a key, data is missing
-    stream
-        .write_all(b"*2\r\n$3\r\nFOO\r\n$5\r\nhello\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 28];
-
-    stream.read_exact(&mut response).await.unwrap();
-
-    assert_eq!(b"-ERR unknown command \'foo\'\r\n", &response);
-}
-
-// In this case we test that server Responds with an Error message if a client
-// sends an GET or SET command after a SUBSCRIBE
-#[tokio::test]
-async fn send_error_get_set_after_subscribe() {
-    let addr = start_server().await;
-
-    let mut stream = TcpStream::connect(addr).await.unwrap();
-
-    // send SUBSCRIBE command
-    stream
-        .write_all(b"*2\r\n$9\r\nsubscribe\r\n$5\r\nhello\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 34];
-
-    stream.read_exact(&mut response).await.unwrap();
-
-    assert_eq!(
-        &b"*3\r\n$9\r\nsubscribe\r\n$5\r\nhello\r\n:1\r\n"[..],
-        &response[..]
-    );
-
-    stream
-        .write_all(b"*3\r\n$3\r\nSET\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 28];
-
-    stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(b"-ERR unknown command \'set\'\r\n", &response);
-
-    stream
-        .write_all(b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n")
-        .await
-        .unwrap();
-
-    let mut response = [0; 28];
-
-    stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(b"-ERR unknown command \'get\'\r\n", &response);
-}
-*/
-
 async fn start_server() -> SocketAddr {
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let addr = listener.local_addr().unwrap();
@@ -341,4 +13,207 @@ async fn start_server() -> SocketAddr {
     tokio::spawn(async move { server::run(listener, tokio::signal::ctrl_c()).await });
 
     addr
+}
+
+async fn start_client(addr: SocketAddr) -> Connection {
+    let mut conn = Connection::new(TcpStream::connect(addr).await.unwrap());
+
+    let info = conn.read_frame().await.unwrap().unwrap();
+
+    match info {
+        Frame::Info {
+            broker_name,
+            nonce: _,
+        } => {
+            assert_eq!(broker_name, "hpfeeds-broker");
+        }
+        _ => panic!("Expected OP_INFO"),
+    }
+
+    // FIXME: Here, need to send an OP_AUTH...
+
+    conn
+}
+
+fn assert_published(
+    frame: &hpfeeds_broker::Result<Option<Frame>>,
+    expected_ident: &str,
+    expected_channel: &str,
+    expected_payload: &Bytes,
+) {
+    match frame {
+        Ok(Some(Frame::Publish {
+            ident,
+            channel,
+            payload,
+        })) => {
+            assert_eq!(ident, expected_ident);
+            assert_eq!(channel, expected_channel);
+            assert_eq!(payload, expected_payload);
+        }
+        Ok(Some(frame)) => {
+            panic!("Received unexpected frame: {}", frame);
+        }
+        Ok(None) => {
+            panic!("No frame received");
+        }
+        Err(e) => {
+            panic!("Received error: {}", e);
+        }
+    }
+}
+
+#[tokio::test]
+async fn pub_sub() {
+    /*
+    Test sending a publish, starting a sub, sending a publish, starting a sub, sending a publish.
+
+    sub1 should see the 2nd and 3rd publish.
+    sub2 should see the 3rd publish.
+    */
+    let addr = start_server().await;
+
+    let mut conn = start_client(addr).await;
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte string"),
+    })
+    .await
+    .unwrap();
+
+    let mut sub1 = start_client(addr).await;
+    sub1.write_frame(&Frame::Subscribe {
+        ident: "sub1".into(),
+        channel: "bar".into(),
+    })
+    .await
+    .unwrap();
+
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte strin2"),
+    })
+    .await
+    .unwrap();
+
+    assert_published(
+        &sub1.read_frame().await,
+        "foo",
+        "bar",
+        &Bytes::from_static(b"this is a byte strin2"),
+    );
+
+    let mut sub2 = start_client(addr).await;
+    sub2.write_frame(&Frame::Subscribe {
+        ident: "sub2".into(),
+        channel: "bar".into(),
+    })
+    .await
+    .unwrap();
+
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte strin3"),
+    })
+    .await
+    .unwrap();
+
+    assert_published(
+        &sub1.read_frame().await,
+        "foo",
+        "bar",
+        &Bytes::from_static(b"this is a byte strin3"),
+    );
+    assert_published(
+        &sub2.read_frame().await,
+        "foo",
+        "bar",
+        &Bytes::from_static(b"this is a byte strin3"),
+    );
+}
+
+#[tokio::test]
+async fn nsubscribe() {
+    /*
+    Test unsubscribing and resubscribing
+    */
+    let addr = start_server().await;
+
+    let mut conn = start_client(addr).await;
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte string"),
+    })
+    .await
+    .unwrap();
+
+    // Start a subscriber
+    let mut sub1 = start_client(addr).await;
+    sub1.write_frame(&Frame::Subscribe {
+        ident: "sub1".into(),
+        channel: "bar".into(),
+    })
+    .await
+    .unwrap();
+
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte strin2"),
+    })
+    .await
+    .unwrap();
+
+    assert_published(
+        &sub1.read_frame().await,
+        "foo",
+        "bar",
+        &Bytes::from_static(b"this is a byte strin2"),
+    );
+
+    // Subscriber unsubscribes
+    sub1.write_frame(&Frame::Unsubscribe {
+        ident: "sub1".into(),
+        channel: "bar".into(),
+    })
+    .await
+    .unwrap();
+
+    // Subscriber misses a publish
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte strin3"),
+    })
+    .await
+    .unwrap();
+
+    // Subscriber subscribes
+    let mut sub1 = start_client(addr).await;
+    sub1.write_frame(&Frame::Subscribe {
+        ident: "sub1".into(),
+        channel: "bar".into(),
+    })
+    .await
+    .unwrap();
+
+    // They should see this publish
+    conn.write_frame(&Frame::Publish {
+        ident: "foo".into(),
+        channel: "bar".into(),
+        payload: Bytes::from_static(b"this is a byte strin4"),
+    })
+    .await
+    .unwrap();
+
+    assert_published(
+        &sub1.read_frame().await,
+        "foo",
+        "bar",
+        &Bytes::from_static(b"this is a byte strin4"),
+    );
 }
