98398a5338476183d602beca101e91e09f951468
John Carr <john.carr@unrouted.co.uk>
2023-11-05 23:44:23 +0000

    Reload on auth changes

--- src/auth.rs
+++ src/auth.rs
@@ -1,11 +1,13 @@
 use anyhow::Result;
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{recommended_watcher, RecommendedWatcher, RecursiveMode, Watcher};
 use serde::Deserialize;
 use sha1::{Digest, Sha1};
 use std::{
     collections::{BTreeMap, BTreeSet},
     path::Path,
+    sync::{Arc, RwLock},
 };
+use tracing::{error, info};
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct User {
@@ -19,16 +21,59 @@ pub struct User {
 pub enum UserSet {
     Static(BTreeMap<String, User>),
     Json {
-        users: BTreeMap<String, User>,
+        users: Arc<RwLock<BTreeMap<String, User>>>,
         watcher: RecommendedWatcher,
     },
 }
 
 impl UserSet {
+    pub fn from_json(path: &str) -> Result<Self> {
+        let path = path.to_string();
+        let watched_path = path.clone();
+
+        let users_string = std::fs::read_to_string(&path)?;
+        let users: BTreeMap<String, User> = serde_json::from_str(&users_string)?;
+        let users = Arc::new(RwLock::new(users));
+        let users_ro_view = users.clone();
+
+        let mut watcher = recommended_watcher(move |res| {
+            info!("Got inotify event: {res:?}");
+
+            let users_string = match std::fs::read_to_string(&path) {
+                Ok(data) => data,
+                Err(e) => {
+                    error!("UserSet: Failed to load path: {path}: {e:?}");
+                    return;
+                }
+            };
+            let new_users: BTreeMap<String, User> = match serde_json::from_str(&users_string) {
+                Ok(data) => data,
+                Err(e) => {
+                    error!("UserSet: Failed to parse json: {path}: {e:?}");
+                    return;
+                }
+            };
+
+            let len = new_users.len();
+
+            let mut guard = users.write().expect("Could not lock user data");
+            *guard = new_users;
+
+            info!("Reloaded user data: {path}. Found {} records.", len);
+        })?;
+
+        watcher.watch(Path::new(&watched_path), RecursiveMode::Recursive)?;
+
+        Ok(UserSet::Json {
+            users: users_ro_view,
+            watcher,
+        })
+    }
+
     pub fn get_user(&self, user: &str) -> Option<User> {
         match self {
             Self::Static(users) => users.get(user).cloned(),
-            Self::Json { users, .. } => users.get(user).cloned(),
+            Self::Json { users, .. } => users.read().unwrap().get(user).cloned(),
         }
     }
 }
@@ -90,18 +135,8 @@ impl Users {
     }
 
     pub fn add_user_set(&mut self, path: String) -> Result<()> {
-        let users_string = std::fs::read_to_string(&path)?;
-        let users: BTreeMap<String, User> = serde_json::from_str(&users_string)?;
-
-        // Automatically select the best implementation for your platform.
-        let mut watcher = notify::recommended_watcher(|res| match res {
-            Ok(event) => println!("event: {:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
-        })?;
-
-        watcher.watch(Path::new(&path), RecursiveMode::Recursive)?;
-
-        self.user_sets.push(UserSet::Json { users, watcher });
+        let user_set = UserSet::from_json(&path)?;
+        self.user_sets.push(user_set);
 
         Ok(())
     }
